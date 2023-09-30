a9671a5525b83f1c169c9ff25c6fcec041c2ffbc
Wilhelm Kraetzig <wkgeithe@t-online.de>
2023-09-30 13:12:47 +0200

    xfce: changed icon theme

--- .scripts/toggle_dark_light/dark_mode.sh
+++ .scripts/toggle_dark_light/dark_mode.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 xfconf-query -c xsettings -p /Net/ThemeName -s "Adapta-Purple-Nokto"
+xfconf-query -c xsettings -p /Net/IconThemeName -s "Zafiro-Icons-Dark"
 kitty +kitten themes --reload-in=all Dracula
--- .scripts/toggle_dark_light/light_mode.sh
+++ .scripts/toggle_dark_light/light_mode.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 xfconf-query -c xsettings -p /Net/ThemeName -s "Adapta-Purple"
+xfconf-query -c xsettings -p /Net/IconThemeName -s "Zafiro-Icons-Light"
 kitty +kitten themes --reload-in=all Adwaita light
