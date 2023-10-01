ee794cf978964b1993a81691358171ae77dfc53d
Wilhelm Kraetzig <wkgeithe@t-online.de>
2023-10-01 11:18:35 +0200

    bugfix: keepass wont follow dark mode

--- .scripts/toggle_dark_light/light_mode.sh
+++ .scripts/toggle_dark_light/light_mode.sh
@@ -3,3 +3,4 @@
 xfconf-query -c xsettings -p /Net/ThemeName -s "Adapta-Purple"
 xfconf-query -c xsettings -p /Net/IconThemeName -s "Zafiro-Icons-Light"
 kitty +kitten themes --reload-in=all Adwaita light
+sed -i -e "s/darker/simple/g" $HOME/.config/qt5ct/qt5ct.conf 
