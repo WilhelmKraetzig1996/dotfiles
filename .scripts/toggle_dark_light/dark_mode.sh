130b3e31a372e97580dee48c94eff88d231744d7
Wilhelm Kraetzig <wkgeithe@t-online.de>
2023-10-18 23:51:41 +0200

    bugfix: kitty theme not switched when other user runs instance

--- .scripts/toggle_dark_light/dark_mode.sh
+++ .scripts/toggle_dark_light/dark_mode.sh
@@ -2,6 +2,7 @@
 
 xfconf-query -c xsettings -p /Net/ThemeName -s "Adapta-Purple-Nokto"
 xfconf-query -c xsettings -p /Net/IconThemeName -s "Zafiro-Icons-Dark"
-kitty +kitten themes --reload-in=all Dracula
+kitty +kitten themes --reload-in=none Dracula
+killall -USR1 kitty
 sed -i -e "s/simple/darker/g" $HOME/.config/qt5ct/qt5ct.conf 
 killall -WINCH nvim
--- .scripts/toggle_dark_light/light_mode.sh
+++ .scripts/toggle_dark_light/light_mode.sh
@@ -2,6 +2,7 @@
 
 xfconf-query -c xsettings -p /Net/ThemeName -s "Adapta-Purple"
 xfconf-query -c xsettings -p /Net/IconThemeName -s "Zafiro-Icons-Light"
-kitty +kitten themes --reload-in=all Adwaita light
+kitty +kitten themes --reload-in=none Adwaita light
+killall -USR1 kitty
 sed -i -e "s/darker/simple/g" $HOME/.config/qt5ct/qt5ct.conf 
 killall -WINCH nvim
