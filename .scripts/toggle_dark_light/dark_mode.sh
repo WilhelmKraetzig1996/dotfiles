f21e28c3762f6534bfefaee4c7e53a21941f2a1d
Wilhelm Kraetzig <wkgeithe@t-online.de>
2023-11-02 19:33:09 +0100

    kitty: changed dark theme

--- .scripts/toggle_dark_light/dark_mode.sh
+++ .scripts/toggle_dark_light/dark_mode.sh
@@ -2,7 +2,7 @@
 
 xfconf-query -c xsettings -p /Net/ThemeName -s "Adapta-Purple-Nokto"
 xfconf-query -c xsettings -p /Net/IconThemeName -s "Zafiro-Icons-Dark"
-kitty +kitten themes --reload-in=none Dracula
+cp $HOME/.config/kitty/dark-theme.conf $HOME/.config/kitty/current-theme.conf
 killall -USR1 kitty
 sed -i -e "s/simple/darker/g" $HOME/.config/qt5ct/qt5ct.conf 
 killall -WINCH nvim
