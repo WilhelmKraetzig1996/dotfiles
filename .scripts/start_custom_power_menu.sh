79cd92440d1f5ec60a0f56793a0cc1eb6d4b63a9
Wilhelm Kraetzig <wkgeithe@t-online.de>
2023-11-15 10:41:02 +0100

    start_custom_power_menu: only minimize slack window instead of closing

--- .scripts/start_custom_power_menu.sh
+++ .scripts/start_custom_power_menu.sh
@@ -2,6 +2,8 @@
 
 if [ "$(xdotool getwindowfocus getwindowname)" = "Desktop" ]; then
     $HOME/.config/rofi/scripts/powermenu_t3
+elif [[ "$(xdotool getwindowfocus getwindowname)" == *"Slack"* ]]; then
+    xdotool getwindowfocus windowminimize
 else
     xdotool getwindowfocus windowclose
 fi
