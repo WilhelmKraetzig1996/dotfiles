6d04bc97bb0505535a041275c40b8a7668810398
Wilhelm Kraetzig <wkgeithe@t-online.de>
2023-11-22 19:54:43 +0100

    build_debug.sh: also build integration tests

--- .scripts/build/build_debug.sh
+++ .scripts/build/build_debug.sh
@@ -1,7 +1,5 @@
 #! /usr/bin/env nix-shell
 #! nix-shell /home/dear/.scripts/build/shell.nix -i bash
-mkdir cmake-build
-conan install --install-folder ./cmake-build .
-cd cmake-build
-cmake -G "Unix Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ ..
+conan install .
+cmake -G "Unix Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=0 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DBUILD_BENCHMARK=true -DBUILD_INTEGRATION_TESTS=true .
 cmake --build . --parallel 7 
