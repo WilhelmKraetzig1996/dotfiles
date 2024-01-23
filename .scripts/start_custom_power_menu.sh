be90d9c06755bad8b3442f6593b34c01ceeda532
Wilhelm Kraetzig <wkgeithe@t-online.de>
2024-01-23 17:15:24 +0100

    start_custom_powermenu: updated for new close button pso

--- .scripts/start_custom_power_menu.sh
+++ .scripts/start_custom_power_menu.sh
@@ -8,7 +8,7 @@ elif [[ "$(xdotool getwindowfocus getwindowname)" == *"Slack"* ]] || [[ "$(xdoto
     win_width=$(xdotool getactivewindow getwindowgeometry | grep Geometry | cut -d " " -f 4 | cut -d "x" -f 1)
 
     close_button_pos_x=$((win_pos_x+win_width-5))
-    close_button_pos_y=$((win_pos_y-30))
+    close_button_pos_y=$((win_pos_y-40))
     prev_mouse_pos_x=$(xdotool getmouselocation | cut -d " " -f 1 | cut -d ":" -f 2)
     prev_mouse_pos_y=$(xdotool getmouselocation | cut -d " " -f 2 | cut -d ":" -f 2)
     xdotool mousemove $close_button_pos_x $close_button_pos_y
