c66127318bfec72be3c0ef4e173a35e660d6888f
Wilhelm Kraetzig <wkgeithe@t-online.de>
2024-01-23 21:25:08 +0100

    updated dark and light theme

--- .scripts/toggle_dark_light/dark_mode.sh
+++ .scripts/toggle_dark_light/dark_mode.sh
@@ -7,6 +7,7 @@ cp $HOME/.config/kitty/dark-theme.conf $HOME/.config/kitty/current-theme.conf
 cp /etc/lightdm/lightdm-gtk-greeter_dark.conf /etc/lightdm/lightdm-gtk-greeter.conf
 cp $HOME/.config/rofi/powermenu/type-3/shared/dark.rasi $HOME/.config/rofi/powermenu/type-3/shared/current.rasi
 cp $HOME/.config/rofi/launchers/type-5/style-4_dark.rasi $HOME/.config/rofi/launchers/type-5/style-4.rasi
+cp $HOME/.config/conky/conky_dark.conf $HOME/.config/conky/conky.conf 
 killall -USR1 kitty
 sed -i -e "s/simple/darker/g" $HOME/.config/qt5ct/qt5ct.conf 
 killall -WINCH nvim
--- .scripts/toggle_dark_light/light_mode.sh
+++ .scripts/toggle_dark_light/light_mode.sh
@@ -2,11 +2,12 @@
 
 xfconf-query -c xsettings -p /Net/ThemeName -s "Flat-Remix-GTK-Blue"
 xfconf-query -c xfwm4 -p /general/theme -s "Flat-Remix-Light-XFWM"
-xfconf-query -c xsettings -p /Net/IconThemeName -s "Flat-Remix-Blue-Light-darkPanel"
-kitty +kitten themes --reload-in=none Adwaita light
+xfconf-query -c xsettings -p /Net/IconThemeName -s "Flat-Remix-Blue-Light"
+cp $HOME/.config/kitty/light-theme.conf $HOME/.config/kitty/current-theme.conf
 cp /etc/lightdm/lightdm-gtk-greeter_light.conf /etc/lightdm/lightdm-gtk-greeter.conf
 cp $HOME/.config/rofi/powermenu/type-3/shared/light.rasi $HOME/.config/rofi/powermenu/type-3/shared/current.rasi 
 cp $HOME/.config/rofi/launchers/type-5/style-4_light.rasi $HOME/.config/rofi/launchers/type-5/style-4.rasi
+cp $HOME/.config/conky/conky_light.conf $HOME/.config/conky/conky.conf 
 killall -USR1 kitty
 sed -i -e "s/darker/simple/g" $HOME/.config/qt5ct/qt5ct.conf 
 killall -WINCH nvim
