37f83356f35cfe0eb9921bb755e11817f0ef4499
Wilhelm Kraetzig <wkgeithe@t-online.de>
2024-02-14 11:44:02 +0100

    custom app closing: updated close button position

--- .scripts/start_custom_power_menu.sh
+++ .scripts/start_custom_power_menu.sh
@@ -7,7 +7,7 @@ elif [[ "$(xdotool getwindowfocus getwindowname)" == *"Slack"* ]] || [[ "$(xdoto
     win_pos_y=$(xdotool getactivewindow getwindowgeometry | grep Position | cut -d " " -f 4 | cut -d "," -f 2)
     win_width=$(xdotool getactivewindow getwindowgeometry | grep Geometry | cut -d " " -f 4 | cut -d "x" -f 1)
 
-    close_button_pos_x=$((win_pos_x+win_width-5))
+    close_button_pos_x=$((win_pos_x+win_width-10))
     close_button_pos_y=$((win_pos_y-40))
     prev_mouse_pos_x=$(xdotool getmouselocation | cut -d " " -f 1 | cut -d ":" -f 2)
     prev_mouse_pos_y=$(xdotool getmouselocation | cut -d " " -f 2 | cut -d ":" -f 2)
