419fe204c83c0425c4bc0d0dce9aba44fd740848
Wilhelm Kraetzig <wkgeithe@t-online.de>
2024-03-12 11:35:39 +0100

    focus mode: bugfix: conky decorated

--- .scripts/focus_mode/toggle_focus_mode.sh
+++ .scripts/focus_mode/toggle_focus_mode.sh
@@ -5,12 +5,12 @@ TOGGLE_FILE=$HOME/.scripts/focus_mode/focus_mode_active
 
 if [ ! -e $TOGGLE_FILE ]; then
 	touch $TOGGLE_FILE
-    echo "(undecorate)" > $HOME/.devilspie/undec.ds
+    sed -i "s/(decorate)/(undecorate)/" $HOME/.devilspie/undec.ds
     xfce4-panel -q
     xfconf-query -c xfce4-notifyd -p /do-not-disturb --create -t bool --set true
 else
 	rm $TOGGLE_FILE
-    echo "(decorate)" > $HOME/.devilspie/undec.ds
+    sed -i "s/(undecorate)/(decorate)/" $HOME/.devilspie/undec.ds
     xfce4-panel &
     xfconf-query -c xfce4-notifyd -p /do-not-disturb --create -t bool --set false
 fi	
