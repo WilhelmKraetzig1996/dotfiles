c1bbb02cc9839fcfe828e5de2c274525d4762f02
Wilhelm Kraetzig <wkgeithe@t-online.de>
2024-03-14 15:40:35 +0100

    ctrl+q: close slack without mouse movements

--- .scripts/start_custom_power_menu.sh
+++ .scripts/start_custom_power_menu.sh
@@ -6,17 +6,8 @@ elif [[ $(xprop -id $(xdotool getactivewindow)| grep WM_CLASS | grep kitty | wc 
     rm /tmp/kittysocket
     xdotool getwindowfocus windowclose
 elif [[ "$(xdotool getwindowfocus getwindowname)" == *"Slack"* ]] || [[ "$(xdotool getwindowfocus getwindowname)" == *"A PipeWire Graph Qt GUI Interface"* ]]; then
-    win_pos_x=$(xdotool getactivewindow getwindowgeometry | grep Position | cut -d " " -f 4 | cut -d "," -f 1)
-    win_pos_y=$(xdotool getactivewindow getwindowgeometry | grep Position | cut -d " " -f 4 | cut -d "," -f 2)
-    win_width=$(xdotool getactivewindow getwindowgeometry | grep Geometry | cut -d " " -f 4 | cut -d "x" -f 1)
-
-    close_button_pos_x=$((win_pos_x+win_width-10))
-    close_button_pos_y=$((win_pos_y-40))
-    prev_mouse_pos_x=$(xdotool getmouselocation | cut -d " " -f 1 | cut -d ":" -f 2)
-    prev_mouse_pos_y=$(xdotool getmouselocation | cut -d " " -f 2 | cut -d ":" -f 2)
-    xdotool mousemove $close_button_pos_x $close_button_pos_y
-    xdotool click --clearmodifiers 1
-    xdotool mousemove $prev_mouse_pos_x $prev_mouse_pos_y
+    xdotool keyup q
+    xdotool key --clearmodifiers ctrl+w
 else
     xdotool getwindowfocus windowclose
 fi
