e122f9cc7e8a4ab6dfec7ca669b7406f7fd27228
Wilhelm Kraetzig <wkgeithe@t-online.de>
2024-12-29 05:48:15 +0100

    build scripts: separate build folders for debug and release

--- .scripts/build/build_debug.sh
+++ .scripts/build/build_debug.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env nix-shell
 #! nix-shell /home/dear/.scripts/build/shell.nix -i bash
-conan install -if ./build .
-cd build
+conan install -if ./build_debug .
+cd build_debug
 cmake -G "Unix Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DBUILD_BENCHMARK=true -DBUILD_INTEGRATION_TESTS=true ..
 cmake --build . --parallel 7 
 cp RUNTIME_OUTPUT_DIRECTORY/* ~/Documents/Pd/externals/
--- .scripts/build/build_release.sh
+++ .scripts/build/build_release.sh
@@ -1,9 +1,8 @@
 #! /usr/bin/env nix-shell
 #! nix-shell /home/dear/.scripts/build/shell.nix -i bash
-conan install -if ./build -s build_type=Release .
-cd build
-cmake -G "Ninja" -DCMAKE_EXPORT_COMPILE_COMMANDS=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DBUILD_BENCHMARK=true -DBUILD_INTEGRATION_TESTS=true ..
+conan install -if ./build_release -s build_type=Release .
+cd build_release
+cmake -G "Ninja" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DBUILD_BENCHMARK=true -DBUILD_INTEGRATION_TESTS=true ..
 cmake --build . --parallel 7 
-cp RUNTIME_OUTPUT_DIRECTORY/* ~/Documents/Pd/externals/
 cd ..
 
